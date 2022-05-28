cfae31397e7a6b7195fa7fef4d29cc10e12e2340
stone <18871162559@163.com>
2022-05-28 11:34:05 +0800

    nb

--- starter_proxy/proxy1_framework.py
+++ starter_proxy/proxy1_framework.py
@@ -30,12 +30,7 @@ def Vod(resource):
         print(resource)
         serverResponse=requests.get('http://localhost:'+str(port)+'/vod/1000'+resource,headers=request.headers,data=request.data)
         return Response(serverResponse)
-def modify_request(message):
-    """
-    Here you should change the requested bit rate according to your computation of throughput.
-    And if the request is for big_buck_bunny.f4m, you should instead request big_buck_bunny_nolist.f4m 
-    for client and leave big_buck_bunny.f4m for the use in proxy.
-    """
+
 
 def request_dns(message):
     # return 8080
