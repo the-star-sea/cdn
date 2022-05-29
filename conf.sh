57233fc886aa8f5e476450164b6020873afe71e4
stone <18871162559@163.com>
2022-05-29 11:24:27 +0800

    nb

--- conf.sh
+++ conf.sh
@@ -7,7 +7,6 @@ ln -s libexpat.so.0.5.0 libexpat.so.0
 rm libapr-1.so.0
 ln -s libapr-1.so.0.5.1 libapr-1.so.0
 pip3 install flask
-cd /home
 nohup python3 dns/dns.py --servers docker_setup/netsim/servers/10servers &
 python3 docker_setup/netsim/netsim.py servers start -s docker_setup/netsim/servers/10servers
 nohup python3 starter_proxy/proxy1.py --filename log1 --a 0.85 &
